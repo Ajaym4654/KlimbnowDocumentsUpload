c5484aae3b3db14e7c76ffdcbf52e2722935a9d8
Ajay Mridha <83244349+Ajaym4654@users.noreply.github.com>
2025-08-10 10:49:18 +0530

    upload.php

--- Backend/upload.php
+++ Backend/upload.php
@@ -4,6 +4,12 @@ header("Access-Control-Allow-Origin: https://klimbnowdocumentsupload.netlify.app
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
