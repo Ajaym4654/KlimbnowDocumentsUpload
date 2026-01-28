def5155e157d5ebda7d957c9ee61a89bddf87ac9
Ajay Mridha <83244349+Ajaym4654@users.noreply.github.com>
2026-01-28 21:18:32 +0530

    Update upload.php

--- Backend/upload.php
+++ Backend/upload.php
@@ -1,17 +1,14 @@
 <?php
-
-// =====================
-// CORS SETTINGS
-// =====================
 header("Access-Control-Allow-Origin: https://klimbnowdocumentsupload.netlify.app");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
+
 
 // =====================
 // PHPMailer Load
